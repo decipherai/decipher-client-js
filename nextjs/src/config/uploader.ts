cbcb53f637dc1ba863f93b49413dcc04765b5c22
Rohan Das <me@rohandas.com>
2024-05-12 17:21:37 -0700

    Update uploader

--- nextjs/src/config/uploader.ts
+++ nextjs/src/config/uploader.ts
@@ -19,37 +19,54 @@ export async function validateAndUploadSourcemaps(
     process.env.DECIPHER_SERVER_URL || "https://www.prod.getdecipher.com";
   try {
     // Find all sourcemap files in the specified path
-    const sourcemaps = glob.sync("**/*.{js,js.map}", {
+    const files = glob.sync("**/*.{js,js.map}", {
       cwd: path,
       nodir: true,
       ignore: "**/node_modules/**/*",
     });
 
-    if (sourcemaps.length === 0) {
+    if (files.length === 0) {
       console.error(
-        "No .js or .js.map files found in the specified directory. Please double check that you have generated sourcemaps for your app and specified the 'path' argument correctly."
+        "[Decipher] Unable to find sourcemaps; something went wrong when processing them."
       );
       return;
     }
+    const sortedFiles = files.sort((a, b) => {
+      if (
+        a.endsWith(".js.map") &&
+        b.endsWith(".js") &&
+        a.slice(0, -7) === b.slice(0, -3)
+      ) {
+        return -1;
+      } else if (
+        b.endsWith(".js.map") &&
+        a.endsWith(".js") &&
+        b.slice(0, -7) === a.slice(0, -3)
+      ) {
+        return 1;
+      }
+      return a.localeCompare(b);
+    });
 
-    // Upload each sourcemap
-    for (const sourcemap of sourcemaps) {
-      const filePath = join(path, sourcemap);
+    const timestamp = new Date().toISOString();
+    for (const file of sortedFiles) {
+      const filePath = join(path, file);
       const fileContent = await readFile(filePath);
       const uploadResponse = await fetch(`${serverUrl}/api/upload_sourcemap`, {
         method: "POST",
         headers: {
           "Content-Type": "application/octet-stream",
           Authorization: `Bearer ${key}`,
-          "X-Filename": encodeURIComponent(sourcemap),
+          "X-Filename": encodeURIComponent(file),
+          "X-Timestamp": timestamp,
         },
         body: fileContent,
       });
       if (!uploadResponse.ok) {
-        console.error(`Failed to upload ${sourcemap}.`);
+        console.error(`[Decipher] Failed to upload ${file}.`);
       }
     }
   } catch (error) {
-    console.error("An error occurred:", error);
+    console.error("[Decipher] An error occurred during sourcmap upload:", error);
   }
 }
