629065719601e893c3e2d614f75ff8821dc7aa38
michael7795 <michael7795@gmail.com>
2024-05-16 23:12:07 -0700

    Fixed base URL

--- frontend/src/session-replay/capture.ts
+++ frontend/src/session-replay/capture.ts
@@ -8,7 +8,7 @@ import {
 } from "@rrweb/types";
 import { DecipherFrontendConfig, User } from "../types/decipher-types";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://www.prod.getdecipher.com";
 const RECORDING_BUFFER_TIMEOUT = 7000;
 
 const win: (Window & typeof globalThis) | undefined =
